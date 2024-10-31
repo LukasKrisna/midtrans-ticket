13f63e6bdfaddd424dbc0fab346f102459d56422
AimLesson <113356516+AimLesson@users.noreply.github.com>
2024-10-31 22:43:44 +0700

    Revert "add migration and apply additional inputs"
    This reverts commit d42ddd4f9f4fd43a912164d5b926e628d7352adc.

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -19,32 +19,23 @@ class OrderController extends Controller
         $request->validate([
             'nama' => 'required|string|max:255',
             'alamat' => 'required|string|max:255',
-            'nomer_whatsapp' => 'required',
-            'nik' => 'required',
-            'gender' => 'required',
+            'nomer_whatsapp' => 'required|string|max:15',
             'email' => 'required|string|email|max:255',
             'price_range' => 'required|integer',
         ]);
 
-        // $user = User::updateOrCreate(
-        //     ['email' => $request->email],
-        //     [
-        //         'name' => $request->nama,
-        //         'alamat' => $request->alamat,
-        //         'nomer_whatsapp' => $request->nomer_whatsapp,
-        //         'nik' => $request->nik,
-        //         'gender' => $request->gender,
-        //     ]
-        // );
+        $user = User::updateOrCreate(
+            ['email' => $request->email],
+            [
+                'name' => $request->nama,
+                'alamat' => $request->alamat,
+                'nomer_whatsapp' => $request->nomer_whatsapp,
+            ]
+        );
 
         $order = Order::create([
-            'user_id' => $request->nik,
+            'user_id' => $user->id,
             'price_range' => $request->price_range,
-            'gender' => $request->gender,
-            'name' => $request->nama,
-            'alamat' => $request->alamat,
-            'nomer_whatsapp' => $request->nomer_whatsapp,
-            'email' => $request->email,
         ]);
 
         // return redirect()->route('process-payment', ['order_id' => $order->id]);
